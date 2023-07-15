56adbdb4c362dc2b42556cd78599700d05f02d8a
Gaurav <gauravpatel3521@gmail.com>
2023-07-16 03:44:33 +0530

    Final designing

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer,ChatContainer,MessageList,Message,MessageInput,TypingIndicator } from "@chatscope/chat-ui-kit-react"; 
-
+import robo from "./robo.png";
 
 const API_KEY=process.env.REACT_APP_API_KEYY;
 console.log(API_KEY);
@@ -75,12 +75,71 @@ function App() {
     })
   }
 
+
+//Js Design typewriter effect.
+  var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
   return (
-   <div className="App">
-    <div style={{position: "relative",height:"800px",width: "700px"}}>
-     <MainContainer>
-      <ChatContainer>
-        <MessageList 
+   <div class="App">
+    
+    <div class="chatbody" style={{height:"400px",width: "500px"}}>
+      <h1 class="dodo-h1">Dodo</h1>
+        <MessageList class="mbox"
         scrollBehavior='smooth'
         typingIndicator={typing ? <TypingIndicator content="Dodo is typing"/>: null}
         >
@@ -89,9 +148,22 @@ function App() {
           })}
         </MessageList>
         <MessageInput placeholder='Type message here' onSend={handleSend} />
-      </ChatContainer>
-     </MainContainer>
+   
     </div>
+        
+<section class="airs">
+<h1 class="dodocall">
+        <a href="" class="typewrite" data-period="2000" data-type='[ "Hi, I am Dodo.", "I am an AI Bot.", "I Love to talk.", "I can do anything.","Gaurav is my developer." ]'>
+          <span class="wrap"></span>
+        </a>
+      </h1>
+      <img src={robo} alt='robot image'/>
+  <div class='air air1'></div>
+  <div class='air air2'></div>
+  <div class='air air3'></div>
+  <div class='air air4'></div>
+  </section>
+
    </div>
   )
 }
